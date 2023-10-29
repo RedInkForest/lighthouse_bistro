a977efdff42c7cda9214a25a93e44fc5cc2b349b
Ethan <Pradhan751957@gmail.com>
2023-10-28 19:15:22 -0500

    map done

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -2,37 +2,32 @@ import * as React from "react";
 import { useState, useEffect, useRef } from "react";
 import "@tomtom-international/web-sdk-maps/dist/maps.css";
 import * as tt from "@tomtom-international/web-sdk-maps";
-import './About.css'
 
-function About() {
+function App() {
   const mapElement = useRef();
-  const mapLongitude = 103.858528;
-  const mapLatitude = 1.282302;
-  const mapZoom = 16
+  const mapLongitude = -121.91599;
+  const mapLatitude = 37.36765;
+  const mapZoom = 13
   const [map, setMap] = useState({});
+
 
   useEffect(() => {
     let map = tt.map({
-      key: process.env.REACT_APP_KEY,
+      key: process.env.TOM_TOM_KEY,
       container: mapElement.current,
       center: [mapLongitude, mapLatitude],
       zoom: mapZoom
     });
-
-    var marker = new tt.Marker().setLngLat([mapLongitude,mapLatitude]).addTo(map)
-
     setMap(map);
+    return () => map.remove();
   }, []);
-  
+
   return (
-    <div className="container">
-      <div className="text">
-        <h1>HOURS</h1>
-      </div>
-      <div ref={mapElement} className="mapDiv"/>
+    <div className="App">
+            <div ref={mapElement} className="mapDiv" />
+        
     </div>
   );
 }
 
-export default About;
-
+export default App;
