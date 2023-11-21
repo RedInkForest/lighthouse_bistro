077fbeb70dcc6ec92b5410ca87b43f1510f51ba7
Ethan <Pradhan751957@gmail.com>
2023-11-20 21:22:20 -0600

    finish the goddam project

---
+++ Back-End/api.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#SQL Setup
+
+
+#GET - reservations for a day
+
+
+#POST - add reservation to database
+
+
+#DELETE - delete reservation
+
+
+#PUT - update reservation information (party size, timing)
